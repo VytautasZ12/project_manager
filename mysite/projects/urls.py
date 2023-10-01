83237e2d9dad3b75c950ed1f9854c8bef5833dfa
VytautasZ12 <vzvytautasz@gmail.com>
2023-10-01 15:21:58 +0300

    Padaryti, kad prisijungusiam vartotojui būtų rodomi jam priskirti projektai (tuos, už kuriuos jis yra atsakingasis).

--- mysite/projects/urls.py
+++ mysite/projects/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
-
+from .views import ProjectListView, ProjectDetailView, UserProjectListView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name="projects"),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name="project"),
+    path('userprojects/', UserProjectListView.as_view(), name= "userprojects")
 ]
--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Project
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -14,3 +15,12 @@ class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = "project.html"
     context_object_name = "project"
+
+
+class UserProjectListView(LoginRequiredMixin, generic.ListView):
+    model = Project
+    template_name = "userprojects.html"
+    context_object_name = 'projects'
+
+    def get_queryset(self):
+        return Project.objects.filter(responsible=self.request.user)
