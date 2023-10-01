7eec1a464b1e3158bb4dd50ba6287c185333db67
VytautasZ12 <vzvytautasz@gmail.com>
2023-10-01 12:29:28 +0300

    Padaryti projektų atvaizdavimo puslapį (ne administravimo puslapyje).

--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Project
 # Create your views here.
+class ProjectListView(generic.ListView):
+    model = Project
+    template_name = "project.html"
+    context_object_name = 'projects'
