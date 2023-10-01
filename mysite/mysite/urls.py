9a9abd172fa6ca0ecfb43acdd9c58b75adb0c4ee
VytautasZ12 <vzvytautasz@gmail.com>
2023-10-01 14:37:45 +0300

    Padaryti, kad prie svetainės būtų leidžiama prisijungti išoriniam vartotojui. Taip pat įgyvendinti slaptažodžio pakeitimą.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('',include('projects.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
